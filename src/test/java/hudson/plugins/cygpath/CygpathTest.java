c7a6059faf61ac2087620d53d4a5b1a2d330e558
kohsuke <kohsuke@dev.java.net>
2009-04-10 01:52:38 +0000

    added a test case

---
+++ src/test/java/hudson/plugins/cygpath/CygpathTest.java
@@ -0,0 +1,34 @@
+package hudson.plugins.cygpath;
+
+import hudson.model.FreeStyleProject;
+import hudson.model.Hudson;
+import hudson.tasks.Shell;
+import org.jvnet.hudson.test.HudsonTestCase;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class CygpathTest extends HudsonTestCase {
+    /**
+     * Verify that it does no harm on Unix.
+     */
+    public void testOnUnix() throws Exception {
+        if(Hudson.isWindows())  return; // can't test
+
+        FreeStyleProject p = createFreeStyleProject();
+        p.getBuildersList().add(new Shell("echo abc"));
+        assertBuildStatusSuccess(p.scheduleBuild2(0).get());
+    }
+
+    /**
+     * Verify that it does the conversion on Cygwin
+     */
+    public void testOnCygwin() throws Exception {
+        if(!Hudson.isWindows())  return; // can't test
+
+        FreeStyleProject p = createFreeStyleProject();
+        p.getBuildersList().add(new Shell("echo abc"));
+        assertBuildStatusSuccess(p.scheduleBuild2(0).get());
+        // TODO: assert that the path is converted to Windows style
+    }
+}
